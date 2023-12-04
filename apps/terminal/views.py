0fa4f5932a3a5a19af61ac95e20a131242465314
miguel garzon <miguel.garzon@momseguros.com>
2023-12-04 13:19:59 -0500

    update main

--- apps/terminal/views.py
+++ apps/terminal/views.py
@@ -5,9 +5,58 @@ from apps.terminal.services.crud_ubicacion import Ubicacion
 from apps.terminal.services.crud_persona import Persona
 from apps.terminal.services.crud_vehiculo import Vehiculo
 from apps.terminal.services.crud_venta import Venta
-
+from django.db import connection
+from collections import namedtuple
+from django.shortcuts import render,redirect
 def index(request):
-    return render(request, 'index.html')
+    with connection.cursor() as cursor:
+     cursor.execute("SELECT COUNT(*) FROM data_venta")
+     columnas = [col[0] for col in cursor.description]  
+     catidad_ventas = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
+     cursor.execute("SELECT COUNT(*) FROM data_vehiculo")
+     columnas = [col[0] for col in cursor.description] 
+     catidad_vehiculo = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
+     cursor.execute("SELECT COUNT(*) FROM data_conductor")
+     columnas = [col[0] for col in cursor.description] 
+     catidad_conductor = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
+     cursor.execute("SELECT COUNT(*) FROM data_usuario")
+     columnas = [col[0] for col in cursor.description] 
+     data_usuario = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
+     cursor.execute("SELECT COUNT(*) FROM data_terminal")
+     columnas = [col[0] for col in cursor.description] 
+     data_terminal = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
+     cursor.execute("SELECT COUNT(*) FROM data_ruta")
+     columnas = [col[0] for col in cursor.description] 
+     data_ruta = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
+     cursor.execute("SELECT COUNT(*) FROM data_servicio")
+     columnas = [col[0] for col in cursor.description] 
+     data_servicio = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
+     cursor.execute("SELECT COUNT(*) FROM data_ciudad")
+     columnas = [col[0] for col in cursor.description] 
+     data_ciudad = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
+
+     cursor.execute("""
+        SELECT data_venta.*, data_estado.*
+        FROM data_venta
+        JOIN data_estado ON data_venta.id_estado_id = data_estado.id
+    """)
+     columnas = [col[0] for col in cursor.description]
+     filas = cursor.fetchall()
+     resultado = [dict(zip(columnas, fila)) for fila in filas]
+     print(resultado)
+ 
+
+     return render(request, 'index.html',{"catidad_ventas":catidad_ventas[0]['count'],
+                                          "cantidad_veh":catidad_vehiculo[0]['count'],
+                                           "catidad_conductor":catidad_conductor[0]['count'],
+                                           "data_usuario":data_usuario[0]['count'],
+                                           "data_terminal":data_terminal[0]['count'],
+                                           "data_ruta":data_ruta[0]['count'],
+                                           "data_ciudad":data_ciudad[0]['count'],
+                                           "data_servicio":data_servicio[0]['count'],
+                                           "data_venta_con":resultado,
+
+                                          })
 def crear_estado(request):
     instancia=Estado()
     return instancia.crearestado(request)
